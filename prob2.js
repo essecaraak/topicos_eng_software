bcb489bf6fcc8deab19d783674d505e37be14b17
Matheus oliveira <matheusoliveiradesouza190503@gmail.com>
2024-04-17 18:30:22 -0300

    alter prob 2

--- prob2.js
+++ prob2.js
@@ -1,6 +1,37 @@
 //https://leetcode.com/problems/roman-to-integer/description/
-function getValue(char) {
-    switch(char) {
+// Define a function named roman_to_Int that converts a Roman numeral to an integer.
+function roman_to_Int(str1) {
+    // Check if str1 is null, if so, return -1.
+    if(str1 == null) return "Não é um numero romano válido";
+    // Initialize the variable num with the integer value of the first character in str1.
+    var num = char_to_int(str1.charAt(0));
+    var pre, curr;
+
+    // Iterate through the characters of str1 starting from the second character.
+    for(var i = 1; i < str1.length; i++){
+        curr = char_to_int(str1.charAt(i)); // Get the integer value of the current character.
+        pre = char_to_int(str1.charAt(i-1)); // Get the integer value of the previous character.
+        // If the current character's value is less than or equal to the previous character's value, add it to num.
+        if(curr==-1){
+            return "Não é um numero romano válido"
+        }
+        if(curr <= pre){
+            num += curr;
+        } else {
+            // If the current character's value is greater than the previous character's value, subtract twice the previous character's value and add the current character's value to num.
+            num = num - pre*2 + curr;
+        }
+    }
+
+    
+        return num;
+    
+    
+}
+
+// Define a function named char_to_int that returns the integer value of a Roman numeral character.
+function char_to_int(c){
+    switch (c){
         case 'I': return 1;
         case 'V': return 5;
         case 'X': return 10;
@@ -8,33 +39,13 @@ function getValue(char) {
         case 'C': return 100;
         case 'D': return 500;
         case 'M': return 1000;
-        default: return -1; // Retorna -1 se o caractere não for reconhecido como numeral romano válido
+        default: return -1; // Return -1 for invalid characters.
     }
 }
 
-function romanToInt(s) {
-    let result = 0;
-    let prevValue = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        const value = getValue(s[i]);
-        if (value == -1 ) {
-            return "não é um numero romano válido"; // Retorna uma mensagem indicando que o numeral romano é inválido
-        }
-        
-        if (value < prevValue) {
-            result -= value;
-        } else if(value > prevValue ) {    
-            result += value;
-        }
-        prevValue = value;
-    }
-
-    return result;
-}
-
-// Testando com "IM"
-console.log(romanToInt("IMA")); // Output: "não é um numero romano válido"
-console.log(romanToInt("III")); // Output: 3
-console.log(romanToInt("LVIII")); // Output: 58
-console.log(romanToInt("MCMXCIV")); // Output: 1994
+// Output the integer value of the Roman numeral 'XXVI' to the console.
+console.log(roman_to_Int('XXVI'));
+// Output the integer value of the Roman numeral 'CI' to the console.
+console.log(roman_to_Int('CI'));
+console.log(roman_to_Int('IM'));
+console.log(roman_to_Int('AB'));
