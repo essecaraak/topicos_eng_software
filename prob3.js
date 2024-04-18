1a2522e370e0e00e417a69195c3e631d8e8ff658
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2024-04-18 13:51:55 -0300

    substituindo problema 3

--- prob3.js
+++ prob3.js
@@ -1,58 +1,26 @@
-//https://leetcode.com/problems/3sum/description/
- // Função auxiliar para evitar duplicatas
-function skipDuplicates(nums, index, direction) {
-    if (direction === 'left') {
-        while (index < nums.length - 1 && nums[index] === nums[index + 1]) {
-            index++;
-        }
-    } else {
-        while (index > 0 && nums[index] === nums[index - 1]) {
-            index--;
+//https://leetcode.com/problems/basic-calculator/?envType=study-plan-v2&envId=top-interview-150
+
+var calculate = function (s) {
+    let ans = 0;
+    let num = 0;
+    let sign = 1;
+    let stack = [sign];
+
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (!isNaN(parseInt(c))) {
+            num = num * 10 + (parseInt(c));
+        } else if (c === '(') {
+            stack.push(sign);
+        } else if (c === ')') {
+            stack.pop();
+        } else if (c === '+' || c === '-') {
+            ans += sign * num;
+            sign = (c === '+' ? 1 : -1) * stack[stack.length - 1];
+            num = 0;
         }
     }
-    return index;
-}
 
-function threeSum(nums) {
-    const result = [];
-    
-    // Ordena o array para facilitar a busca de duplas
-    nums.sort((a, b) => a - b);
-    
-    for (let i = 0; i < nums.length - 2; i++) {
-        // Evita duplicatas para o primeiro número
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
-        
-        let left = i + 1;
-        let right = nums.length - 1;
-        
-        while (left < right) {
-            const sum = nums[i] + nums[left] + nums[right];
-            
-            if (sum === 0) {
-                result.push([nums[i], nums[left], nums[right]]);
-                
-                // Evita duplicatas para o segundo número
-                left = skipDuplicates(nums, left, 'left');
-                
-                // Evita duplicatas para o terceiro número
-                right = skipDuplicates(nums, right, 'right');
-                
-                left++;
-                right--;
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-    }
-    
-    return result;
+    return ans + sign * num;
 }
-
-// Testes
-console.log(threeSum([-1, 0, 1, 2, -1, -4])); // Output: [[-1, -1, 2], [-1, 0, 1]]
-console.log(threeSum([0, 1, 1])); // Output: []
-console.log(threeSum([0, 0, 0])); // Output: [[0, 0, 0]]
 
