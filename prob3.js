80dac808dae69e243bdc13686f13a7329ec7f1e7
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2024-04-19 18:15:23 -0300

    atualizando o problema

--- prob3.js
+++ prob3.js
@@ -1,26 +1,37 @@
-//https://leetcode.com/problems/basic-calculator/?envType=study-plan-v2&envId=top-interview-150
-
 var calculate = function (s) {
-    let ans = 0;
-    let num = 0;
-    let sign = 1;
-    let stack = [sign];
-
+    s = "(" + s + ")"
+    let stack = [];
+    let temp = [];
     for (let i = 0; i < s.length; i++) {
-        const c = s[i];
-        if (!isNaN(parseInt(c))) {
-            num = num * 10 + (parseInt(c));
-        } else if (c === '(') {
-            stack.push(sign);
-        } else if (c === ')') {
+        if (s[i] === " ") continue;
+        if (s[i] === ")") {
+            while (stack[stack.length - 1] !== "(") temp.push(stack.pop());
             stack.pop();
-        } else if (c === '+' || c === '-') {
-            ans += sign * num;
-            sign = (c === '+' ? 1 : -1) * stack[stack.length - 1];
-            num = 0;
+            stack.push(count(temp));
+            continue;
         }
+        if (isNum(stack[stack.length - 1]) && isNum(s[i])) {
+            stack[stack.length - 1] += s[i];
+            continue;
+        }
+        if (s[i] === "-" || s[i] === "+") {
+            if (stack.length === 0 || stack[stack.length - 1] === "(") stack.push("0");
+        }
+        stack.push(s[i]);
     }
+    return stack[0];
+};
 
-    return ans + sign * num;
+function count(temp) {
+    temp = temp.reverse();
+    while (temp.length !== 1) {
+        if (temp[1] === "+") temp[0] = (+temp[0]) + (+temp[2]);
+        if (temp[1] === "-") temp[0] = (+temp[0]) - (+temp[2]);
+        temp.splice(1, 2);
+    }
+    return temp.pop();
 }
 
+function isNum(str) {
+    return /[0-9]+/.test(str);
+}
